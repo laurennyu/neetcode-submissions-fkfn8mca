876768871cc488342079d9a5c6439ba264c242e8
Lauren Yu <120423740+laurennyu@users.noreply.github.com>
2026-07-07 13:13:59 -0400

    Add: task-scheduling - submission-3

---
+++ Data Structures & Algorithms/task-scheduling/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        from collections import Counter
+        import numpy as np
+        freq = Counter(tasks)
+        freq_list = list(freq.values())
+
+        # Get most frequent item
+        max_f = max(freq_list)
+        # Count how much "overflow" occurs (if multiple tasks have max freq)
+        max_count = 0
+        for count in freq_list:
+            if count == max_f:
+                max_count += 1
+
+        # Calculate space needed for the tasks if schedule not completely full
+        res = ((max_f - 1) * (n + 1)) + max_count
+
+        return max(res, len(tasks))
