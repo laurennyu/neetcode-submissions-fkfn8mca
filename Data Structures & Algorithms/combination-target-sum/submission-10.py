41cbeda7434345337179d4e6a9a40ab7788c283d
Lauren Yu <120423740+laurennyu@users.noreply.github.com>
2026-09-12 18:05:39 -0400

    Add: combination-target-sum - submission-10

---
+++ Data Structures & Algorithms/combination-target-sum/submission-10.py
@@ -0,0 +1,37 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+
+        res = []
+
+        def solve(candidate: list, idx: int, curr_sum: int):
+            nonlocal nums, res, target
+
+            # if curr_sum == target:
+            #     res.append(candidate.copy())
+            #     return
+            
+            # if curr_sum > target or idx >= len(nums):
+            #     return
+            if idx >= len(nums):
+                return
+
+            # There are two options: pick curr num or don't pick curr num
+            curr_num = nums[idx]
+            if curr_sum + curr_num == target:
+                # Choose num and stop exploring- any subsequent num will be bigger than curr_num
+                res.append(candidate.copy() + [curr_num])
+                return
+
+            elif curr_sum + curr_num < target:
+                # Choose num and keep exploring
+                candidate.append(curr_num)
+                solve(candidate, idx, curr_sum + curr_num)
+                candidate.pop()
+
+            # Don't choose num
+            solve(candidate, idx + 1, curr_sum)
+
+        solve([], 0, 0)
+
+        return res
